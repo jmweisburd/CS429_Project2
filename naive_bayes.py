fca0140f43186a873a561f1530011af88e0c5e25
Julian Weisburd <jmweisburd@gmail.com>
2016-03-24 14:00:37 -0600

    B:

--- naive_bayes.py
+++ naive_bayes.py
@@ -6,7 +6,10 @@ from entropy import *
 train_doc_total = 11269
 v_total = 61188
 
-beta = 1/(v_total)
+#beta = 1/(v_total)
+#alpha = (1+beta)
+
+beta = math.pow(10, -5)
 alpha = (1+beta)
 
 beta_vector = []
@@ -26,8 +29,8 @@ accuracy_vector = []
 p_y = [0.04259472890229834, 0.05155736977549028, 0.05075871860857219, 0.05208980388676901, 0.051024935664211554, 0.052533498979501284, 0.051646108794036735, 0.052533498979501284, 0.052888455053687104, 0.0527109770165942, 0.05306593309078002, 0.0527109770165942, 0.05244475996095483, 0.0527109770165942, 0.052622237998047744, 0.05315467210932647, 0.04836276510781791, 0.05004880646020055, 0.04117490460555506, 0.033365870973467035]
 
 
-tm = training_matrix()
-nb_c = classifier(tm, p_y)
+#tm = training_matrix()
+#nb_c = classifier(tm, p_y)
 
 #nb_c.makeMAPMatrix(alpha)
 #ent = entropy(nb_c, p_y)
@@ -54,8 +57,7 @@ while b != -5:
     accur = nb_c.calculateAccuracy(False)
     print(accur)
     accuracy_vector.append(accur)
-    b = b - 0.05
+    b = b - 0.25
 
-
-#print(alpha_vector)
-#print(accuracy_vector)
+print(beta_vector)
+print(accuracy_vector)
